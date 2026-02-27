c8f810198293ff58490918d6c4c70e9e5e84e0e9
Nia <hakase@litdevs.org>
2026-02-27 12:58:28 +0100

    log tilefile errors properly

--- src/scanner.ts
+++ src/scanner.ts
@@ -146,8 +146,8 @@ export default class Scanner extends EventEmitter<ScannerEvents> {
             let tileFile;
             try {
                 tileFile = await fetch(`https://backend.wplace.live/tile/${coords[0]}/${coords[1]}.png`, {signal: AbortSignal.timeout(5*1000)});
-            } catch {console.warn(`Couldn't download tile "${coords[0]} ${coords[1]}".`); continue;}
-            if(!tileFile.ok) {console.warn(`Couldn't download tile "${coords[0]} ${coords[1]}".`); continue;}
+            } catch(e) {console.error(e); continue;}
+            if(!tileFile.ok) {console.warn(`HTTP ${tileFile.status} for "${coords[0]} ${coords[1]}".`); continue;}
 
             tileCount++;
             const tileSharp = sharp(await tileFile.arrayBuffer());
